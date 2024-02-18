efe89db28097114ee907974d78280c5955bd626e
ISTIEAK AHMED <54891541+istieak-a@users.noreply.github.com>
2024-02-18 23:31:11 +0600

    Add new routes for About, Services, and Contact components

--- src/main.jsx
+++ src/main.jsx
@@ -10,17 +10,24 @@ import {
 } from "react-router-dom";
 import Layout from "./Layout.jsx";
 import Home from "./components/Home/Home.jsx";
+import About from "./components/About/About.jsx";
+import Services from "./components/Services/Services.jsx";
+import Contact from "./components/Contact/Contact.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route path="" element={<Home />} />
-    </Route>
-  )
+      <Route path="/about" element={<About />} />
+      <Route path="/services" element={<Services/>} />
+      <Route path="/contact" element={<Contact/>} />
+
+    </Route>,
+  ),
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </React.StrictMode>,
 );

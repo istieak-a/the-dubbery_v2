f862bbb3bdadc70fbedd90b239a98fe9694de92c
ISTIEAK AHMED <54891541+istieak-a@users.noreply.github.com>
2024-02-18 20:15:26 +0600

    Add PortfolioCard component and update Home.jsx and home.css

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,6 +7,9 @@ import ButtonAccent from "../Button/ButtonAccent";
 import dubbing_mic from "../../assets/dubbing_mic.jpg";
 import ServiceCard from "../ServiceCard/ServiceCard";
 import { RiArrowRightSLine } from "react-icons/ri";
+import PortfolioCard from "../PortfolioCard/PortfolioCard";
+import mrBeast from "../../assets/mr_beast.jpg";
+import Button from "../Button/Button";
 
 const Home = () => {
   const serviceData = [
@@ -27,6 +30,38 @@ const Home = () => {
       title: "Seamless Distribution Across Multiple Platforms",
       desc: "We ensure that your content is effectively distributed across various platforms, maximizing its reach and impact.",
       btn: "Learn More",
+    },
+  ];
+  const youtubers = [
+    {
+      img: mrBeast,
+      title: "PewDiePie",
+      desc: "Swedish gamer and comedian known for his Let's Play videos and meme-filled content.",
+    },
+    {
+      img: mrBeast,
+      title: "Markiplier",
+      desc: "American gamer popular for playthroughs of indie and horror games, often with a comedic focus.",
+    },
+    {
+      img: mrBeast,
+      title: "MrBeast",
+      desc: "American philanthropist and YouTuber known for elaborate stunts and large-scale giveaways.",
+    },
+    {
+      img: mrBeast,
+      title: "whinderssonnunes",
+      desc: "Brazilian comedian and vlogger with a massive following in the Portuguese-speaking world.",
+    },
+    {
+      img: mrBeast,
+      title: "Emma Chamberlain",
+      desc: "American lifestyle vlogger known for her relatable humor, candid editing style, and coffee obsession.",
+    },
+    {
+      img: mrBeast,
+      title: "Ryan Higa (Nigahiga)",
+      desc: "Japanese-American comedian, actor, and YouTube personality known for his skits and parodies.",
     },
   ];
   return (
@@ -106,7 +141,7 @@ const Home = () => {
       </section>
       <section className="section_2 flex w-full flex-col items-center justify-center gap-10 bg-bg100 px-10 py-20 text-text100 md:flex-row md:px-[8vw]">
         <div className="flex w-full flex-col items-start gap-5 md:w-[70%]">
-          <h2 className="font-bold text-sm">Empower</h2>
+          <h2 className="text-sm font-bold">Empower</h2>
           <h1 className="text-2xl font-bold text-accent200 md:text-4xl 2xl:text-5xl ">
             Unlock Your Earning Potential with Ad Revenue Sharing
           </h1>
@@ -135,12 +170,34 @@ const Home = () => {
             <button className="rounded-xl border-2 border-accent100 px-5 py-2 text-sm font-semibold">
               Learn More
             </button>
-            <button className="flex items-center gap-2">Sign Up <RiArrowRightSLine /></button>
+            <button className="flex items-center gap-2">
+              Sign Up <RiArrowRightSLine />
+            </button>
           </div>
         </div>
         <div className="flex w-full cursor-pointer items-center justify-center  overflow-hidden rounded-2xl bg-bg100 transition-all duration-300 hover:scale-105 md:w-1/2">
           <img className="h-full w-full" src={dubbing_mic} alt="" />
         </div>
+      </section>
+      <section className="flex min-h-screen flex-col gap-10 items-center bg-bg100 px-10 py-20 md:px-20">
+        <div className="portfolio-title-section flex w-full flex-col items-center rounded-md py-10 text-center text-white">
+          <h2 className="text-xs font-semibold">PORTFOLIO</h2>
+          <h1 className="text-4xl font-bold md:text-5xl">Cerators Go Global</h1>
+          <p className="text-xs">Check our projects that we have collborated</p>
+        </div>
+
+        <div className="project_cards_container mt-10 grid grid-cols-1 gap-16 md:grid-cols-2 lg:grid-cols-3">
+          {youtubers.map((youtuber, index) => (
+            <PortfolioCard
+              key={index}
+              img={youtuber.img}
+              title={youtuber.title}
+              desc={youtuber.desc}
+              className="project-card"
+            />
+          ))}
+        </div>
+        <Button>View All</Button>
       </section>
     </div>
   );
---
+++ src/components/ReviewCard/ReviewCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ReviewCard = () => {
+  return (
+    <div>
+      This is review Card
+    </div>
+  )
+}
+
+export default ReviewCard
